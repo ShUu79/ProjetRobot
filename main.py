16d01248e1c01da482ba78678e26f25e147a5136
ShUu79 <epechadre79@gmail.com>
2024-04-30 09:03:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+#salut DJi80
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
